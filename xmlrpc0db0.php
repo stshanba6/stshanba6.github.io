7e0dba644a40a4f9dccbc28e5a9eebb0a4bf70dd
62574 <62574@ONE>
2026-03-07 14:09:06 +0800

    a

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,12 +2,12 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>https://dogedoog.wordpress.com</homePageLink>
+    <homePageLink>https://googdog0.wordpress.com</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="https://dogedoog.wordpress.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://dogedoog.wordpress.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://dogedoog.wordpress.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="https://dogedoog.wordpress.com/xmlrpc.php" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://googdog0.wordpress.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://googdog0.wordpress.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://googdog0.wordpress.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://googdog0.wordpress.com/xmlrpc.php" />
       <api name="Twitter" blogID="" preferred="false" apiLink="https://twitter-api.wordpress.com/" />
     </apis>
   </service>
